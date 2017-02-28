24892b37121a1fae11766f78e7438aae5a77d292
Rodrigo Reyes <rodrigo.reyes@laureate.net>
2017-02-28 10:21:28 -0500

    radar works.

--- app/js/main.js
+++ app/js/main.js
@@ -1,121 +1,60 @@
-var map;
-var dataContent;
-var jsonFile='js/data.json';
-var markers={}
+//////////////////////////////////////////////////////////////
+//////////////////////// Set-Up //////////////////////////////
+//////////////////////////////////////////////////////////////
 
-function init() {
- map= L.map('map', {
-   attributionControl: false,
-        minZoom:0,
-        maxZoom:2,
-        zoom:-1,
-         zoomControl: false,
-        crs: L.CRS.Simple
-    }).setView([-450, 912], 1);
-    var w=3000,
-        h=1500,
-        url='images/big_map.jpg';
-        // calculate the edges of the image, in coordinate space
-  var southWest = map.unproject([0, h], map.getMaxZoom()-1);
-  var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
-  var bounds = new L.LatLngBounds(southWest, northEast);
-  L.imageOverlay(url, bounds).addTo(map);
-  map.setMaxBounds(bounds);
-  map.dragging.disable();
-   map.touchZoom.disable();
-   map.doubleClickZoom.disable();
-   map.scrollWheelZoom.disable();
+var margin = {top: 100, right: 100, bottom: 100, left: 100},
+  width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right,
+  height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
 
-}
+//////////////////////////////////////////////////////////////
+////////////////////////// Data //////////////////////////////
+//////////////////////////////////////////////////////////////
 
+var data = [
+      [//iPhone
+      {axis:"Battery Life",value:0.22},
+      {axis:"Brand",value:0.28},
+      {axis:"Contract Cost",value:0.29},
+      {axis:"Design And Quality",value:0.17},
+      {axis:"Have Internet Connectivity",value:0.22},
+      {axis:"Large Screen",value:0.02},
+      {axis:"Price Of Device",value:0.21},
+      {axis:"To Be A Smartphone",value:0.50}
+      ],[//Samsung
+      {axis:"Battery Life",value:0.27},
+      {axis:"Brand",value:0.16},
+      {axis:"Contract Cost",value:0.35},
+      {axis:"Design And Quality",value:0.13},
+      {axis:"Have Internet Connectivity",value:0.20},
+      {axis:"Large Screen",value:0.13},
+      {axis:"Price Of Device",value:0.35},
+      {axis:"To Be A Smartphone",value:0.38}
+      ],[//Nokia Smartphone
+      {axis:"Battery Life",value:0.26},
+      {axis:"Brand",value:0.10},
+      {axis:"Contract Cost",value:0.30},
+      {axis:"Design And Quality",value:0.14},
+      {axis:"Have Internet Connectivity",value:0.22},
+      {axis:"Large Screen",value:0.04},
+      {axis:"Price Of Device",value:0.41},
+      {axis:"To Be A Smartphone",value:0.30}
+      ]
+    ];
+//////////////////////////////////////////////////////////////
+//////////////////// Draw the Chart //////////////////////////
+//////////////////////////////////////////////////////////////
 
-$( document ).ready(function() {
-  $('.closeBtn').hide();
-  init ();
-  $.getJSON(jsonFile, function (data) {
-    var template = document.getElementById('templatePanel').innerHTML;
-     var html = Mustache.to_html(template, data);
-     $('#accordion').append(html);
-    for (var i in data.markers) {
-      var xPos= data.markers[i].xPos;
-      var yPos= data.markers[i].yPos;
-      var id= data.markers[i].id;
-       //createMarker(xPos, yPos,id);
-    }
-  }).done(function() {
-    console.log ('json ready');
-    $('#healthCenter').collapse('toggle');
-     $('.pannel-side').toggleClass('pannel-side__closed')
+var color = d3.scale.ordinal()
+  .range(["#EDC951","#CC333F","#00A0B0"]);
 
-  })
-
-
-  $('body').on('click', '.item', function(){
-    var frame= $(this).data('iframe');
-     var type= $(this).data('type');
-     switch(type) {
-       case "link":
-          if (frame==true){
-            $(".extContainer").attr("src",$(this).data('url') );
-            $(".extContainer").animate({top:0}, 500, function() {})
-              //$(".extContainer").toggleClass("extContainer--open")
-              $('.closeBtn').fadeIn(300);
-          }
-           break;
-           case "document":
-           var url="documents/"+$(this).data('url');
-           window.open(url);
-
-             break;
-       case "video":
-         break;
-       }
-  });
-
-
-});
-
-
-
-
-$('.closeBtn').click(function() {
-  $('.closeBtn').hide();
-  var delta=$( window ).height();
-  console.log (delta+"<<<")
-  $(".extContainer").animate({top:delta}, 500, function() {
-    $(".extContainer").attr("src",null );
-      $('.closeBtn').fadeOut(300);
-  })
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-function createMarker (xPos,yPos,id){
-  var coordinates= map.unproject( [xPos,yPos] )
-  var marker = L.marker(coordinates).on('click', onClick);
-  marker.id=id;
-  marker.addTo(map);
-  console.log ('marker Created')
+var radarChartOptions = {
+  w: width,
+  h: height,
+  margin: margin,
+  maxValue: 0.5,
+  levels: 5,
+  roundStrokes: true,
+  color: color
 };
-
-
-function onClick(e) {
-$('#'+e.target.id).collapse('toggle')
-map.setView([e.latlng.lat, e.latlng.lng], 1);
-
-if ($('.pannel-side').hasClass('pannel-side__closed'))
-{
-   $('.pannel-side').toggleClass('pannel-side__closed');
-};
-
-};
+//Call function to draw the Radar chart
+RadarChart(".radarChart", data, radarChartOptions);
